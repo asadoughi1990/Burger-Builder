bf82344f7e79787dbe06596913be77416cd71449
Ali Sadoughi <asadoughi1990@gmail.com>
2020-05-16 20:31:18 +0430

    Added Build Controls

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,29 +1,52 @@
 import React, { Component } from "react";
 
 import Burger from "../../components/Burger/Burger";
+import BuildControls from "../../components/Burger/BuildControls/BuildControls";
+
+const IGREDIENT_PRICES = {
+  salad: 0.5,
+  cheese: 0.4,
+  meat: 1.3,
+  bacon: 0.7,
+};
 
 class BurgerBuilder extends Component {
-    // constructor(props) {
-    //     super(props);
-    //     this.state = {...}
-    // }
-    state = {
-        ingredients: {
-            salad: 0,
-            bacon: 0,
-            cheese: 0,
-            meat: 0
-        }
-    };
+  // constructor(props) {
+  //     super(props);
+  //     this.state = {...}
+  // }
+  state = {
+    ingredients: {
+      salad: 0,
+      bacon: 0,
+      cheese: 0,
+      meat: 0,
+    },
+    totalPrice: 4,
+  };
 
-    render() {
-        return (
-            <React.Fragment>
-                <Burger ingredients={this.state.ingredients} />
-                <div>Build Controls</div>
-            </React.Fragment>
-        );
-    }
+  addIngredientHandler = (type) => {
+    const updatedCount = this.state.ingredients[type] + 1;
+    const newPrice = this.state.totalPrice + IGREDIENT_PRICES[type];
+    this.setState({
+      ingredients: {
+        ...this.state.ingredients,
+        [type]: updatedCount,
+      },
+      totalPrice: newPrice,
+    });
+  };
+
+  removeIngredientHandler = (type) => {};
+
+  render() {
+    return (
+      <React.Fragment>
+        <Burger ingredients={this.state.ingredients} />
+        <BuildControls ingredientAdded={this.addIngredientHandler} />
+      </React.Fragment>
+    );
+  }
 }
 
 export default BurgerBuilder;
